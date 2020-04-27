aa7887629a0dea676612e284c4b9c715091134b6
Konstantin S <havendv@gmail.com>
2020-04-28 03:40:11 +1000

    Updated packages to latest

--- sources/Application/ViewModels/MainWindowViewModel.cs
+++ sources/Application/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MultiFactorAuthentication.ViewModels
+﻿namespace MultiFactorAuthentication.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
